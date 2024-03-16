eff53279dc4662f487456598697aa5d82fd2a303
Alec DiFederico <alecdifederico@gmail.com>
2024-03-16 10:55:31 -0400

    got core logic working properly

--- duties.go
+++ duties.go
@@ -1,24 +1,69 @@
 package main
 
+import "fmt"
+
+// Simple wrapper struct for an epoch/slot pair
+type EpochSlot struct {
+	Slot  uint64
+	Epoch uint64
+}
+
 type ValidatorDuties struct {
-	Slots map[uint64][]uint64
+	EpochSlots []EpochSlot
+	Validator  uint64
 }
 
 // Create a new instance of validator duties
-func New() *ValidatorDuties {
+func New(validator uint64) *ValidatorDuties {
 	// Create a new validator duties struct
 	validatorDuties := &ValidatorDuties{
-		Slots: make(map[uint64][]uint64),
+		Validator:  validator,
+		EpochSlots: []EpochSlot{},
 	}
 
 	return validatorDuties
 }
 
 // Processes an attester duty response
-func (d *ValidatorDuties) ProcessDuties(duties AttesterDutiesResponse) {
+func (d *ValidatorDuties) ProcessDuties(epoch uint64) {
+	// Query the attester duties from the beacon node
+	epochDuties := QueryAttesterDuties(epoch, d.Validator)
+
 	// Pull out the data from the response
-	for _, epochData := range duties.Data {
-		// At the slot, append the validator index
-		d.Slots[epochData.Slot] = append(d.Slots[epochData.Slot], epochData.ValidatorIndex)
+	for _, epochData := range epochDuties.Data {
+		// add the slot for the validator
+		d.EpochSlots = append(d.EpochSlots, EpochSlot{epochData.Slot, epoch})
 	}
+}
+
+// Queries the head block of the beacon chain
+func QueryBeaconHeadBlock() SignedBeaconBlockResponse {
+	// Signed beacon block response
+	var signedBeaconBlock SignedBeaconBlockResponse
+
+	// Request the head block of the beacon chain
+	err := GetRequest("beacon/headers/head", &signedBeaconBlock)
+	if err != nil {
+		panic(err)
+	}
+
+	return signedBeaconBlock
+}
+
+// Queries the duties of the attester for a given epoch
+func QueryAttesterDuties(epoch uint64, validator uint64) AttesterDutiesResponse {
+	// Attester duties response
+	var epochDuties AttesterDutiesResponse
+
+	// Request the attester duties for the epoch
+	err := PostRequest(
+		fmt.Sprintf("validator/duties/attester/%d", epoch),
+		&epochDuties,
+		[]uint64{validator},
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	return epochDuties
 }
--- main.go
+++ main.go
@@ -4,38 +4,8 @@ import (
 	"fmt"
 	"math"
 	"strconv"
-	"strings"
 	"time"
 )
-
-func QueryBeaconHeadBlock() SignedBeaconBlockResponse {
-	// Signed beacon block response
-	var signedBeaconBlock SignedBeaconBlockResponse
-
-	// Request the head block of the beacon chain
-	err := GetRequest("beacon/headers/head", &signedBeaconBlock)
-	if err != nil {
-		panic(err)
-	}
-
-	return signedBeaconBlock
-}
-
-func QueryAttesterDuties(epoch uint64, validators []uint64) AttesterDutiesResponse {
-	var epochDuties AttesterDutiesResponse
-
-	// Request the attester duties for the epoch
-	err := PostRequest(
-		fmt.Sprintf("validator/duties/attester/%d", epoch),
-		&epochDuties,
-		validators,
-	)
-	if err != nil {
-		panic(err)
-	}
-
-	return epochDuties
-}
 
 func main() {
 	// Get the head block of the beacon chain
@@ -47,19 +17,13 @@ func main() {
 	// Determine current epoch using current slot
 	currentEpoch := currentSlot / SlotsPerEpoch
 
-	// Validators whose duties should be retrieved
-	validators := []uint64{811475}
+	// Create a validator duties manager with the validator whose duties should
+	// be retrieved
+	dutiesManager := New(811475)
 
-	// Create a validator duties manager
-	DutiesManager := New()
-
-	// Validator duties responses
-	currEpochDuties := QueryAttesterDuties(currentEpoch, validators)
-	nextEpochDuties := QueryAttesterDuties(currentEpoch+1, validators)
-
-	// Process each response
-	DutiesManager.ProcessDuties(currEpochDuties)
-	DutiesManager.ProcessDuties(nextEpochDuties)
+	// Process the duties for the current epoch and the next epoch
+	dutiesManager.ProcessDuties(currentEpoch)
+	dutiesManager.ProcessDuties(currentEpoch + 1)
 
 	// Load the specified timezone. Default to UTC
 	loc, _ := time.LoadLocation("America/New_York")
@@ -68,46 +32,39 @@ func main() {
 	// the current time.
 	prevAttestEnd := time.Now().In(loc)
 
+	// Display the validator
+	fmt.Printf("Validator: %s\n", strconv.Itoa(int(dutiesManager.Validator)))
+
 	// For each slot in the mapping, get the timestamp of the slot start
-	for s, validators := range DutiesManager.Slots {
+	for _, epochSlot := range dutiesManager.EpochSlots {
 		// Determine the timestamp the slot started
-		slotStart := time.Unix(int64(BeaconChainGenesis+s*SecondsPerSlot), 0)
+		slotStart := time.Unix(int64(BeaconChainGenesis+epochSlot.Slot*SecondsPerSlot), 0)
 
 		// Gap until the next attestion must be made
 		gapUntilNextAttest := int64(math.Floor(slotStart.Sub(prevAttestEnd).Seconds()))
-
-		// If there are validators at this slot, list those validators
-		var validatorStrs []string
-		for _, v := range validators {
-			validatorStrs = append(validatorStrs, strconv.Itoa(int(v)))
-		}
 
 		// If the attestation has already occurred in the current epoch, then it
 		// can be skipped
 		if gapUntilNextAttest < 0 {
 			// Display output if already attested
 			fmt.Printf(
-				"slot %d - already attested at this epoch - Validators: %s\n",
-				s,
-				strings.Join(validatorStrs, ", "),
+				"epoch %d - slot %d - already attested at this epoch\n",
+				epochSlot.Epoch,
+				epochSlot.Slot,
 			)
 		} else {
 			// Display output if havent yet attested
 			fmt.Printf(
-				"slot %d - gap of %d seconds - from %s to %s - Validators: %s\n",
-				s,
+				"epoch %d - slot %d - gap of %d seconds - from %s to %s\n",
+				epochSlot.Epoch,
+				epochSlot.Slot,
 				gapUntilNextAttest,
 				prevAttestEnd.Format(time.Kitchen),
 				slotStart.Format(time.Kitchen),
-				strings.Join(validatorStrs, ", "),
 			)
 		}
 
 		// Set the previous attestion time
 		prevAttestEnd = slotStart
 	}
-
-	// TODO: I should add the epoch in the output as well
-	// TODO: I should make the validator the key in the mapping. Then, for each validator
-	// we can loop through and calculate the unique gaps for each one.
 }
